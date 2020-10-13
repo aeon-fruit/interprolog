ae232a41226e47296e8f8be52a68bb8465e594b6
Atef N <aeon.fruit@email.com>
2020-10-14 00:06:11 +0200

    Rollback to version 2.5.x

--- otherInterPrologDoc/PrologInstall/xsb/For_build_windows/custom_settings.mak
+++
@@ -1,4 +0,0 @@
-XSB_INTERPROLOG=yes
-MY_INCLUDE_DIRS=/I"C:\Programas\Java\jdk1.7.0_04\include" /I"C:\Programas\Java\jdk1.7.0_04\include\win32" /I"C:\Users\mc\Desktop\Pre-built\include"
-MY_LIBRARY_DIRS="C:\Users\mc\Desktop\Pre-built\lib\pthreadVC2.lib"
-
---
+++ test/com/xsb/interprolog/XSBNativeEngineTest.java
@@ -0,0 +1,47 @@
+/* 
+** Author(s): Miguel Calejo
+** Contact:   interprolog@declarativa.com, http://www.declarativa.com, http://www.xsb.com
+** Copyright (C) XSB Inc., USA, 2001-2005, Declarativa 2013
+** Use and distribution, without any warranties, under the terms of the 
+** GNU Library General Public License, readable in http://www.fsf.org/copyleft/lgpl.html
+*/
+package com.xsb.interprolog;
+import com.declarativa.interprolog.AbstractPrologEngine;
+
+public class XSBNativeEngineTest extends NativeEngineTest{
+	public XSBNativeEngineTest(String name){super(name);}
+	protected AbstractPrologEngine buildNewEngine(){
+		//String dir = new XSBPeer().getBinDirectoryProperty(System.getProperties());
+		if (singleEngine==null){
+			NativeEngine.JNIinSameThread=true;
+			singleEngine = new NativeEngine(/*dir*/);
+			loadTestFile(singleEngine);
+		}
+		return singleEngine;
+	}
+    protected void setUp() throws java.lang.Exception{
+		engine.command("import append/3,length/2 from basics");		
+    }
+    protected void tearDown() throws java.lang.Exception{
+    }
+    
+	// XSB 2.7.1 has float problems on Linux:
+	public void testNumbers2(){
+		if (AbstractPrologEngine.isWindowsOS()||AbstractPrologEngine.isMacOS())
+			super.testNumbers2();
+		else System.err.println("Skipping testNumbers2");
+	}
+	public void testNumbers(){
+		if (AbstractPrologEngine.isWindowsOS()||AbstractPrologEngine.isMacOS())
+			super.testNumbers();
+		else System.err.println("Skipping testNumbers2");
+	}
+	// THIS TIMES OUT but only for NativeEngine. Doesn't matter threadedCallbacks:
+	public void testGoalThreadedCB(){
+		System.err.println("Skipped testGoalThreadedCB in "+engine);
+	}
+    public void testGoalSameThreadCB(){
+		System.err.println("Skipped testGoalSameThreadCB in "+engine);
+	}
+
+}
